58f98b949ee86055435facbb82e44a278f2c1b03
Yosef Shulman <yosef.shulman@protonmail.com>
2022-12-09 13:07:07 +0200

    Body Parser Added

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get("/", (request, response) => {
-  response.send("Hello world!");
+  response.send(request.body);
 });
 
 app.listen(5000);
